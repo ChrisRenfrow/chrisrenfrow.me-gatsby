c370cfe467050081a1cbc40a7f40da897dbc6b7b
Chris Renfrow <mail@chrisrenfrow.me>
2019-01-16 14:57:47 -0800

    Testing gh-pages deploy through circleci

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,6 @@ import Layout from "../components/layout"
 
 export default ({ data }) => (
     <Layout pageTitle="About">
-        <p>This is where I'll talk all about myself.</p>
         <p>Coming soon!</p>
         <p><Link to="/">Go Home</Link></p>
     </Layout>
