62db03469af958cdb9964adb96e36a64cfb7014e
Chris Renfrow <dev@chrisrenfrow.me>
2019-08-28 17:05:43 -0700

    Duration of entries now calculated and displayed; contact info aligned by row/col; tweaks and typo fixes

--- src/components/resume.js
+++ src/components/resume.js
@@ -66,7 +66,7 @@ const Skill = props => (
 const ContactMethod = props => (
   <div
     css={{
-      margin: '5px 10px',
+      margin: '5px 20px',
       height: '50px',
       width: 'auto'
     }}
@@ -88,22 +88,39 @@ const ContactMethod = props => (
   </div>
 )
 
+// TODO: Move some of this out as a utility/helper
+// TODO: If month diff < 12, show month diff instead of years
 const DateRange = props => {
   let range = ''
+  let { start, end } = props
+  let startDate = new Date(start);
+  let endDate = (end) ? new Date(end) : new Date()
+  let diffYears = 0;
+  let dateOptions = { year: 'numeric', month: 'short' }
 
-  if (props.start) {
-    if (props.end) {
-      if (String(props.start) === String(props.end)) {
-        range = `${ props.start }`
-      } else {
-        range = `${ props.start } - ${ props.end }`
-      }
-    } else {
-      range = `${ props.start } - Present`
-    }
+  if (startDate && endDate) {
+    let diffTime = Math.abs(startDate.getTime() - endDate.getTime())
+    let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+    let diffMonths = Math.ceil(diffDays / (7 * 4))
+    diffYears = Math.ceil(diffMonths / 12)
+  }
+
+  if (start && end) {
+    if (String(start) === String(end))
+      range = `${ startDate.toLocaleDateString("en-US", dateOptions) }`
+    else
+      range = `${ startDate.toLocaleDateString("en-US", dateOptions) } - ` +
+        `${ endDate.toLocaleDateString("en-US", dateOptions) }`
+  } else if (start) {
+    range = `${ startDate.toLocaleDateString("en-US", dateOptions) } - Present`
   } else {
     return null
   }
+  
+  range += (diffYears !== 0) ? 
+    ` (${diffYears} ${(diffYears > 1) ? 'years' : 'year'})`
+    : ""
+
   return (<h4 css={{ marginTop: rhythm(0) }}>{ range }</h4>)
 }
 
@@ -115,6 +132,7 @@ const Entry = props => (
     <h5
       css={{ marginTop: rhythm(0) }}
     >{props.subtitle}</h5>
+    { console.log(`Parsing ${props.title} - ${props.subtitle} start and end dates`)}
     <DateRange start={props.start} end={props.end} />
     { md.render(props.body) }
   </div>
@@ -126,33 +144,55 @@ export default ({ data }) => (
       title="Summary"
       body={
         `Hey there! I'm a software developer with over 2 years of professional experience working remotely on full-stack solutions for clients with big ideas. In my free time you'll find me planning my break into security, preparing for the next [DEF CON](https://defcon.org/), or figuring out a way to automate something in my day-to-day life.
-        \nI am currently open to additional additional opportunities/clients. If you are interested in the services I provide please feel free to reach out to me using one of the [methods](#contact) below.`
+        \nI am currently open to additional opportunities/clients. If you are interested in the services I provide please feel free to reach out to me using one of the [methods](#contact) below.`
       }
     />
 
     <ResumeContainer
       title="Contact"
     >
-      <div css={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        alignItems: 'center',
-        justifyContent: 'space-between'
+      <div style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        width: "100%",
+        justifyContent: "center",
+        alignContent: "center",
       }}>
-        <ContactMethod
-          title="Email"
-          value="dev@chrisrenfrow.me"
-          link="mailto:dev@chrisrenfrow.me"
-        />
-        <ContactMethod
-          title="Keybase"
-          value="crenfrow"
-          link="https://keybase.io/crenfrow"
-        />
-        <ContactMethod
-          title="Mobile/Signal"
-          value="+1 (805) 270-3252"
-        />
+        <div style={{
+          display: "flex",
+          flexDirection: "column",
+          flexBasis: "100%",
+          flex: 1
+        }}>
+          <ContactMethod
+            title="Email"
+            value="dev@chrisrenfrow.me"
+            link="mailto:dev@chrisrenfrow.me"
+          />
+          <ContactMethod
+            title="Keybase"
+            value="crenfrow"
+            link="https://keybase.io/crenfrow"
+          />
+        </div>
+        <div style={{
+          display: "flex",
+          flexDirection: "column",
+          flexBasis: "100%",
+          flex: 1
+        }}>
+          <ContactMethod
+            title="LinkedIn"
+            value="crenfrow"
+            link="https://www.linkedin.com/in/crenfrow"
+          />
+          <ContactMethod
+            title="Mobile/Signal"
+            value="+1 (805) 270-3252"
+            link="tel:+18052703252"
+          />
+        </div>
       </div>
     </ResumeContainer>
     <ResumeContainer
@@ -161,17 +201,18 @@ export default ({ data }) => (
       <div css={{
         display: 'flex',
         flexWrap: 'wrap',
+        alignItems: 'center',
         justifyContent: 'center'
       }}>
-        <Skill title="Node.js" percentage=".75" />
-        <Skill title="Firebase" percentage=".75" />
-        <Skill title="C" percentage=".75" />
-        <Skill title="Bash" percentage=".75" />
-        <Skill title="GNU/Linux" percentage=".75" />
-        <Skill title="React" percentage=".5" />
-        <Skill title="Python" percentage=".5" />
-        <Skill title="Golang" percentage=".5" />
-        <Skill title="Security" percentage=".33" />
+        <Skill title="Node.js"    percentage=".75" />
+        <Skill title="Firebase"   percentage=".75" />
+        <Skill title="C"          percentage=".75" />
+        <Skill title="Bash"       percentage=".75" />
+        <Skill title="GNU/Linux"  percentage=".75" />
+        <Skill title="React"      percentage=".50" />
+        <Skill title="Python"     percentage=".50" />
+        <Skill title="Golang"     percentage=".50" />
+        <Skill title="Security"   percentage=".33" />
       </div>
     </ResumeContainer>
     <ResumeContainer
@@ -181,7 +222,7 @@ export default ({ data }) => (
       <Entry
         title="Software Developer"
         subtitle="Findable"
-        start="July 2019"
+        start="2019-07"
         body={
           `A platform that makes renting to or leasing from anyone as easy and painless as it should be.
           \nLearn more at [findable.net](https://findable.net)`
@@ -191,7 +232,7 @@ export default ({ data }) => (
       <Entry
         title="Software Developer"
         subtitle="MCrowdsourcing Canada (MC2)"
-        start="Sep 2017"
+        start="2017-09"
         body={
           `MC2 is a social venture that seeks to transform the way we do common altruistic tasks by providing a platform for those that want to contribute by taking positive actions, and those that want to sponsor and encourage them to do so.
           \nDuring my time at MC2 I've been primarily responsible for the continuing development and maintenance of MC2's Facebook chatbot service (built with Node.js) among other data-related tasks.`
@@ -215,13 +256,14 @@ export default ({ data }) => (
         title="ChrisRenfrow.me"
         start="2018"
         body={
-          `My personal website and blog. Made with React and Gatsby.js to achieve all the performance benefits of a static website while still using modern web-development standards.`
+          `My personal website and blog. Made with React and Gatsby.js to achieve all the performance benefits of a static website while still using modern web-development standards.
+          \nCheck it out on [github](https://github.com/ChrisRenfrow/chrisrenfrow.me)`
         }
       />
       <Entry
         title="Interactive AI Storyteller"
-        start="2017"
-        end="2017"
+        start="2017-07"
+        end="2017-07"
         body={
           `One of the winning projects made during the 2017 VentureBeat AI Hackathon which made use of Google Assistant to create an interactive story-telling experience.
           \nMade using Firebase Cloud Functions and DialogFlow (for conversational intent-matching).`
@@ -233,8 +275,8 @@ export default ({ data }) => (
     >
       <Entry
         title="42 Silicon Valley"
-        start="2016"
-        end="2018"
+        start="2016-08"
+        end="2018-04"
         body={
           `42 is a nonprofit, tuition-free programming school created and funded by French billionare and philanthropist, Xavier Niel. This 3-5 year program departs from the traditional education experience by using a peer-to-peer based learning system with no classes or teachers and a purely project-based curriculum. To learn more, visit [42.us.org](https://www.42.us.org).
           \nI worked with my peers on projects involving computer graphics and low level computing. Went on to learn about web development using PHP during an intense web development program. And quickly achieved my first internship.`
