2be430fd8bc4daf8509ba07345992fe16fa8732f
Chris Renfrow <dev@chrisrenfrow.me>
2020-08-05 12:31:11 -0700

    Fix linting issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,63 +1,63 @@
 module.exports = {
   siteMetadata: {
-    title: `Chris Renfrow`,
-    author: `Chris Renfrow`
+    title: 'Chris Renfrow',
+    author: 'Chris Renfrow'
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `Chris Renfrow`,
-        short_name: `Chris Renfrow`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#fff`,
-        display: `minimal-ui`,
-        icon: `content/assets/simple-logo.png`,
+        name: 'Chris Renfrow',
+        short_name: 'Chris Renfrow',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#fff',
+        display: 'minimal-ui',
+        icon: 'content/assets/simple-logo.png',
       }
     },
-    `gatsby-plugin-offline`,
+    'gatsby-plugin-offline',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `src`,
+        name: 'src',
         path: `${ __dirname }/src/`,
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `assets`,
+        name: 'assets',
         path: `${ __dirname }/content/assets`
       }
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `blog`,
+        name: 'blog',
         path: `${ __dirname }/content/blog`
       }
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
-          `gatsby-remark-images`,
-          `gatsby-remark-responsive-iframe`,
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`
+          'gatsby-remark-images',
+          'gatsby-remark-responsive-iframe',
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files'
         ]
       }
     },
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-emotion`,
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-transformer-remark',
+    'gatsby-plugin-emotion',
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        pathToConfigModule: 'src/utils/typography',
       },
     },
   ],
